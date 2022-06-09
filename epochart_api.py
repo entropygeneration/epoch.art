4618bbab36f095e5e9c83d652573a383c8502c43
3ntr0py <84992259+entropygeneration@users.noreply.github.com>
2022-06-08 20:07:53 -0400

    Update epochart_api.py

--- epochart_api.py
+++ epochart_api.py
@@ -84,30 +84,3 @@ def get_sales(status, policy_id, date):
 		print(r.text)
 
 base_url = 'https://api.epoch.art'
-policy_id = 'd0112837f8f856b2ca14f69b375bc394e73d146fdadcc993bb993779'
-asset_id = 'd0112837f8f856b2ca14f69b375bc394e73d146fdadcc993bb993779446973636f536f6c6172697330383335'
-yesterday = (datetime.date.today() - datetime.timedelta(days = 1)).strftime("%m/%d/%Y")
-
-#r = get_quote(base_url, 'ADA')
-#r = get_assets(base_url, policy_id)
-#r = get_base_collection(base_url, policy_id)
-#r = get_collection(base_url, policy_id, 1, 100, '', 'recent', 1)
-#r = get_image(policy_id, asset_id)
-
-#r = get_sales('sold', policy_id, yesterday)
-r = get_sales('sold', policy_id, '')
-#print(r)
-
-for item in r:
-	print(item['_id'])
-	print(item['sellerStakeAddress'])
-	print(item['sellerPaymentAddress'])
-	print(item['asset'])
-	print(item['assetName'])
-	print(item['policyId'])
-	print(item['price'])
-	print(item['status'])
-	print(item['creationDate'])
-	if 'buyDate' in item:
-		print(item['buyDate'])
-	print('')
